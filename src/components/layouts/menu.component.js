639961c7262e635e49219271b522ddbc3412ec81
Aurangzaib Najam <Aurangzaib Najam>
2022-10-18 23:44:14 +0500

    Resolves linting issue in menu component

--- src/components/layouts/menu.component.js
+++ src/components/layouts/menu.component.js
@@ -1,18 +1,19 @@
-import {Routes, Route} from "react-router-dom";
-import NavBar from "./navbar.component";
-import BlogList from "../blogs/blog-list.component";
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import NavBar from './navbar.component';
+import BlogList from '../blogs/blog-list.component';
 
-function Menu(){
-    return <div>
-        <NavBar />
-        <div>
-            <Routes>
-                <Route path="/" element={<BlogList />} />
-                <Route path="/most-liked" />
-                <Route path="/most-commented" />
-            </Routes>
-        </div>
-  </div>
+function Menu() {
+  return <div>
+    <NavBar />
+    <div>
+      <Routes>
+        <Route path="/" element={<BlogList />} />
+        <Route path="/most-liked" />
+        <Route path="/most-commented" />
+      </Routes>
+    </div>
+  </div>;
 }
 
 export default Menu;
