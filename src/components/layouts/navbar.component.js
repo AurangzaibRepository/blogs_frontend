3e6be8cf31189fe1757717d056973249ffca65b1
Aurangzaib Najam <Aurangzaib Najam>
2022-10-19 00:44:37 +0500

    Resolves eslint

--- src/components/layouts/navbar.component.js
+++ src/components/layouts/navbar.component.js
@@ -1,19 +1,20 @@
-import {Link} from "react-router-dom";
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 function NavBar() {
-    return  <nav className="navbar navbar-expand navbar-dark bg-dark">
-            <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-                <Link to="/" className="nav-link">Blogs</Link>
-            </li>
-            <li className="nav-item">
-                <Link to="/most-liked" className="nav-link">Most liked</Link>
-            </li>
-            <li className="nav-item">
-                <Link to="/most-commented" className="nav-link">Most Commented</Link>
-            </li>
-            </ul>
-    </nav>
+  return <nav className="navbar navbar-expand navbar-dark bg-dark">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item">
+        <Link to="/" className="nav-link">Blogs</Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/most-liked" className="nav-link">Most liked</Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/most-commented" className="nav-link">Most Commented</Link>
+      </li>
+    </ul>
+  </nav>;
 }
 
 export default NavBar;
