5c745e52b0a79b278898b3a049476cfd24af6672
Aurangzaib Najam <Aurangzaib Najam>
2022-10-24 22:10:41 +0500

    Implements most liked components

---
+++ src/components/blogs/most-liked.component.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Title from '../layouts/title.component';
+
+class MostLikedBlogs extends Component {
+  render() {
+    return (
+      <div>
+        <Title title={'Most Liked Blogs'}></Title>
+      </div>
+    );
+  }
+}
+
+export default MostLikedBlogs;
