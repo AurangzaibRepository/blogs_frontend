41ead6e106f4e88c47c65fc2b4c6e404d1ca919b
Aurangzaib Najam <Aurangzaib Najam>
2022-10-24 22:03:07 +0500

    Implements prop types

--- src/components/layouts/title.component.js
+++ src/components/layouts/title.component.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {Helmet} from 'react-helmet';
+import PropTypes from 'prop-types';
 
 function Title(props) {
   return <Helmet>
     <title>{props.title}</title>
   </Helmet>;
 }
+
+Title.propTypes = {
+  title: PropTypes.string,
+};
 
 export default Title;
