24654971dc1e375115193cf67acabe5726a9c325
Aurangzaib Najam <Aurangzaib Najam>
2022-10-24 22:18:09 +0500

    Implements most commented blogs componetn

--- src/components/blogs/most-commented.component.js
+++ src/components/blogs/most-commented.component.js
@@ -1,2 +1,14 @@
 import React, {Component} from 'react';
 import Title from '../layouts/title.component';
+
+class MostCommentedBlogs extends Component {
+  render() {
+    return (
+      <div>
+        <Title title={'Most Commented Blogs'}></Title>
+      </div>
+    );
+  }
+}
+
+export default MostCommentedBlogs;
