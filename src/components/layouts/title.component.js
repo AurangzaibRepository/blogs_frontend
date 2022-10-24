ac7899d61bf642636aa003eead5150155a7f405a
Aurangzaib Najam <Aurangzaib Najam>
2022-10-24 21:48:49 +0500

    Imports react in jsx

--- src/components/layouts/title.component.js
+++ src/components/layouts/title.component.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import {Helmet} from 'react-helmet';
 
 function Title(props) {
   return <Helmet>
     <title>{props.title}</title>
-  </Helmet>,
+  </Helmet>;
 }
 
 export default Title;
