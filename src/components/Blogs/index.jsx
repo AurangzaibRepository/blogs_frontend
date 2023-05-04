e524070f9620fcd8b9a57769cf9ac31063af1550
Aurangzaib Najam <Aurangzaib Najam>
2023-05-05 01:59:32 +0500

    Amends Blogs component extenssion

---
+++ src/components/Blogs/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Title from '../layouts/Title';
+
+function Blogs() {
+  return (
+    <div>
+      <Title title="Blogs" />
+    </div>
+  );
+}
+
+export default Blogs;
