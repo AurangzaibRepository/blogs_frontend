b3d4ec15f92dce6c196d804e9189aac89a0fa8d4
Aurangzaib Najam <Aurangzaib Najam>
2023-05-05 21:06:47 +0500

    Adds state

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Title from '../layouts/Title';
 
 function About() {
+  const [aboutData, setAboutData] = useState();
+
   return (
     <>
       <Title title="About" />
       <div className="container dv-main">
         { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
         <label className="title full-width text-center">About Me</label>
+        <p>{aboutData}</p>
       </div>
     </>
   );
