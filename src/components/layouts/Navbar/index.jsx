a284f8b538a31cbeba0d00bb2346b69b4290bd3a
Aurangzaib Najam <Aurangzaib Najam>
2023-05-05 20:35:28 +0500

    Wraps logo inside link

--- src/components/layouts/Navbar/index.jsx
+++ src/components/layouts/Navbar/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Menu from './Menu';
 import logo from '../../../assets/logo.png';
 import './Style.css';
@@ -6,7 +7,9 @@ import './Style.css';
 function Navbar() {
   return (
     <div id="dv-topmenu">
-      <img src={logo} alt="logo" />
+      <Link to="/">
+        <img src={logo} alt="logo" />
+      </Link>
       <Menu />
     </div>
   );
