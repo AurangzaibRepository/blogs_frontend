40ab4ca331c622739055b01546fffe6ef6652cf7
Aurangzaib Najam <Aurangzaib Najam>
2023-05-05 20:19:10 +0500

    Creates about component

---
+++ src/components/About/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Title from '../layouts/Title';
+
+function About() {
+  return (
+    <Title title="About" />
+  );
+}
+
+export default About;
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Title from '../layouts/Title';
+
+function Home() {
+  return (
+    <Title title="Home" />
+  );
+}
+
+export default Home;
