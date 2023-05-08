e9bda33dccc0f90b1873e2d0e1e3f547745f2675
Aurangzaib Najam <Aurangzaib Najam>
2023-05-08 20:46:25 +0500

    Creates what use component

---
+++ src/components/About/WhatUseSection.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function WhatUseSection() {
+  return (
+    
+  );
+}
+
+export default WhatUseSection;
