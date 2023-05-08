f2d08bcbcb9f6938f92d4d2259148c4bae5a4ee2
Aurangzaib Najam <Aurangzaib Najam>
2023-05-08 18:31:19 +0500

    Implements prop types

--- src/components/About/SupportSection.jsx
+++ src/components/About/SupportSection.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function SupportSection() {
+function SupportSection({
+    body,
+    followMe,
+    leaveReview,
+    share,
+    learnReact,
+    contribute,
+    donate
+}) {
 
 }
+
+SupportSection.propTypes = {
+  body: PropTypes.string.isRequired,
+  followMe: PropTypes.string.isRequired,
+  leavrReview: PropTypes.string.isRequired,
+  share: PropTypes.string.isRequired,
+  learnReact: PropTypes.string.isRequired,
+  contribute: PropTypes.string.isRequired,
+  donate: PropTypes.string.isRequired,
+};
 
 export default SupportSection;
