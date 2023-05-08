7f0a109186c85c53dd54c58e824b8f49527bc0d4
Aurangzaib Najam <Aurangzaib Najam>
2023-05-08 21:11:20 +0500

    Adds props

--- src/components/About/WhatUseSection.jsx
+++ src/components/About/WhatUseSection.jsx
@@ -2,9 +2,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function WhatUseSection() {
+function WhatUseSection({
+  body,
+  digitalOcean,
+  email,
+  technicalGear,
+  apps,
+  freelenacers,
+  newsletter,
+  emailMarketing,
+}) {
   return (
-    
+
   );
 }
 
