e71ed7f0d19aa7a07c0928949f74e4ea74e402a9
Aurangzaib Najam <Aurangzaib Najam>
2023-05-08 19:34:05 +0500

    Declares prop types

--- src/components/About/PassionsSection.jsx
+++ src/components/About/PassionsSection.jsx
@@ -1,11 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
 import ContentsSection from './ContentsSection';
 
 function PassionsSection({
-    body,
-    watching,
-    noteworthy,
+  body,
+  watching,
+  noteworthy,
 }) {
   return (
     <ContentsSection
@@ -15,5 +16,11 @@ function PassionsSection({
     />
   );
 }
+
+PassionsSection.propTypes = {
+  body: PropTypes.string.isRequired,
+  watching: PropTypes.instanceOf(Array).isRequired,
+  noteworthy: PropTypes.instanceOf(Array).isRequired,
+};
 
 export default PassionsSection;
