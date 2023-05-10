f69aaa392aa231aa02264e7cce888acf9b089225
Aurangzaib Najam <Aurangzaib Najam>
2023-05-10 23:39:47 +0500

    Adds body to brand icon component

--- src/components/Footer/BrandIcon.jsx
+++ src/components/Footer/BrandIcon.jsx
@@ -3,9 +3,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function BrandIcon({ url, icon }) {
+function BrandIcon({
+  url,
+  icon,
+  className
+}) {
   return (
-
+    <a href={url}>
+      <FontAwesomeIcon icon={icon} className={className} />
+    </a>
   );
 }
 
