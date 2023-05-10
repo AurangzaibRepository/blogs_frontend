49a26576b6de427b3a475b23acb86ade7f35a077
Aurangzaib Najam <Aurangzaib Najam>
2023-05-10 20:37:06 +0500

    Adds footer elements

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,9 +3,16 @@ import './style.css';
 
 function Footer() {
   return (
-    <div id="dv-footer">
+    <div id="dv-footer" className="d-flex justify-content-between">
       { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
       <label className="small">© Robin Wieruch</label>
+      <div>
+        icons
+      </div>
+      <div>
+        <a href="mailto:hello@gmail.com" className="small me-4">Contact Me</a>
+        <a href="/legal" className="small">Privacy & Terms</a>
+      </div>
     </div>
   );
 }
