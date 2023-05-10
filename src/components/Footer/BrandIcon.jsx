f5919485e6a5ccdbd301148e86fb36eb4bf915a5
Aurangzaib Najam <Aurangzaib Najam>
2023-05-10 22:48:54 +0500

    Creates brand icon component

---
+++ src/components/Footer/BrandIcon.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function BrandIcon({ url, icon }) {
+  return (
+
+  );
+}
+
+export default BrandIcon;
