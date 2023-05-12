095f75e7c6d908016147200ae756cb92f8a19c9d
Aurangzaib Najam <Aurangzaib Najam>
2023-05-12 19:31:53 +0500

    Includes footer contents component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,40 +6,44 @@ import {
   faFacebook,
   faTwitter,
 } from '@fortawesome/free-brands-svg-icons';
+import FooterContents from './FooterContents';
 import BrandIcon from './BrandIcon';
 import './style.css';
 
 function Footer() {
   return (
-    <div id="dv-footer" className="d-flex justify-content-between">
-      { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-      <label className="small">© Robin Wieruch</label>
-      <div>
-        <BrandIcon
-          url="http://www.twitter.com"
-          className="me-4"
-          icon={faTwitter}
-        />
-        <BrandIcon
-          url="http://www.github.com"
-          className="me-4"
-          icon={faGit}
-        />
-        <BrandIcon
-          url="http://www.facebook.com"
-          className="me-4"
-          icon={faFacebook}
-        />
-        <BrandIcon
-          url="mailto:test@gmail.com"
-          icon={faMailBulk}
-        />
+    <>
+      <FooterContents />
+      <div id="dv-footer" className="d-flex justify-content-between">
+        { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
+        <label className="small">© Robin Wieruch</label>
+        <div>
+          <BrandIcon
+            url="http://www.twitter.com"
+            className="me-4"
+            icon={faTwitter}
+          />
+          <BrandIcon
+            url="http://www.github.com"
+            className="me-4"
+            icon={faGit}
+          />
+          <BrandIcon
+            url="http://www.facebook.com"
+            className="me-4"
+            icon={faFacebook}
+          />
+          <BrandIcon
+            url="mailto:test@gmail.com"
+            icon={faMailBulk}
+          />
+        </div>
+        <div>
+          <a href="mailto:hello@gmail.com" className="small me-4">Contact Me</a>
+          <a href="/legal" className="small">Privacy & Terms</a>
+        </div>
       </div>
-      <div>
-        <a href="mailto:hello@gmail.com" className="small me-4">Contact Me</a>
-        <a href="/legal" className="small">Privacy & Terms</a>
-      </div>
-    </div>
+    </>
   );
 }
 
