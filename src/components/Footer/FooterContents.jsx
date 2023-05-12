b10f1998f1a034cb959df9061a7404c69b46ed30
Aurangzaib Najam <Aurangzaib Najam>
2023-05-12 20:50:44 +0500

    Includes props

--- src/components/Footer/FooterContents.jsx
+++ src/components/Footer/FooterContents.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function FooterContents() {
+function FooterContents({ portfolioLinks, aboutLinks }) {
   return (
     <div className="container" id="dv-footer-contents">
       <div className="row">
         <div className="col-md-6 text-center">
           { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
           <label className="sub-title">PORTFOLIO</label>
+          <ul>
+           
+          </ul>
         </div>
         <div className="col-md-6 text-center">
           { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
           <label className="sub-title">ABOUT</label>
+          <ul>
+            
+          </ul>
         </div>
       </div>
     </div>
