8917a9406c5cba5a2544211a427316d14a5e66c1
Aurangzaib Najam <Aurangzaib Najam>
2023-05-12 20:24:40 +0500

    Specifies id and class

--- src/components/Footer/FooterContents.jsx
+++ src/components/Footer/FooterContents.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
 function FooterContents() {
-  return(
-
+  return (
+    <div className="container" id="dv-footer-contents">
+      <div className="row">
+        <div className="col-md-6 text-center">
+          { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
+          <label className="sub-title">PORTFOLIO</label>
+        </div>
+        <div className="col-md-6 text-center">
+          { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
+          <label className="sub-title">ABOUT</label>
+        </div>
+      </div>
+    </div>
   );
-};
+}
 
 export default FooterContents;
