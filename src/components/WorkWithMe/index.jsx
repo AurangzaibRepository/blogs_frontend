bcd5b86135457103dc231b6d8b112855ec184ae1
Aurangzaib Najam <Aurangzaib Najam>
2023-05-12 23:30:58 +0500

    Includes contents section component

--- src/components/WorkWithMe/index.jsx
+++ src/components/WorkWithMe/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import ContentsSection from '../About/ContentsSection';
 import Title from '../layouts/Title';
 
 function WorkWithMe() {
   return (
     <>
-     <Title title="React Developer" />
+      <Title title="React Developer" />
+      <div className="container dv-main">
+      </div>
     </>
   );
 }
