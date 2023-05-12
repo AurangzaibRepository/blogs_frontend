ad6a0aec02a9d0952de4e207f9aa91a55364a93b
Aurangzaib Najam <Aurangzaib Najam>
2023-05-13 01:05:56 +0500

    Implements function to retreive data from json file

--- src/services/WorkWithMeService.jsx
+++ src/services/WorkWithMeService.jsx
@@ -1,5 +1,10 @@
 const WorkWithMeService = {
+  getData: async () => {
+    let data = await fetch('data/workwithme_data.json');
+    data = data.json();
 
+    return data;
+  },
 };
 
 export default WorkWithMeService;
