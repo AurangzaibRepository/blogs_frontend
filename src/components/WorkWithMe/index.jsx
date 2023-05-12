26d515242fa66e7617e5bf86ce08e03ca90eae53
Aurangzaib Najam <Aurangzaib Najam>
2023-05-13 01:07:52 +0500

    Includes service

--- src/components/WorkWithMe/index.jsx
+++ src/components/WorkWithMe/index.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ContentsSection from '../About/ContentsSection';
+import WorkWithMeService from '../../services/WorkWithMeService';
 import Title from '../layouts/Title';
 
 function WorkWithMe() {
