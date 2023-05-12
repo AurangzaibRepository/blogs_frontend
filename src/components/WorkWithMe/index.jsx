79d1e5af4f8f64d3428d3aac0099a7604a6fea42
Aurangzaib Najam <Aurangzaib Najam>
2023-05-12 23:23:11 +0500

    Includes title component

--- src/components/WorkWithMe/index.jsx
+++ src/components/WorkWithMe/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Title from '../layouts/Title';
 
 function WorkWithMe() {
   return (
+    <>
+     <Title title="React Developer" />
+    </>
   );
-};
+}
 
 export default WorkWithMe;
