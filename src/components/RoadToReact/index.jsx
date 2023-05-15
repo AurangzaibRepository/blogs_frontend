42b2a0ba0fca55a0c99c57445eaa0aa0a824cee3
Aurangzaib Najam <Aurangzaib Najam>
2023-05-15 22:40:48 +0500

    Includes image for react book

--- src/components/RoadToReact/index.jsx
+++ src/components/RoadToReact/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import imgReactBook from '../../assets/react-book.png';
 
 function RoadToReact() {
   return (
     <div className="row">
-      <div className="col-md-6"></div>
+      <div className="col-md-6">
+        <img src={imgReactBook} alt="react book" />
+      </div>
       <div className="col-md-6"></div>
     </div>
   );
