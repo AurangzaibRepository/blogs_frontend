545409a1afefcda8d106c56179870854c4002d6d
Aurangzaib Najam <Aurangzaib Najam>
2023-05-15 20:10:22 +0500

    Defines prop types

--- src/components/WorkWithMe/ContentsSection.jsx
+++ src/components/WorkWithMe/ContentsSection.jsx
@@ -11,9 +11,16 @@ function ContentsSection({
   return (
     <>
       { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-      <label className={"full-width"}></label>
+      <label className={`full-width ${titleClass}`}>{title}</label>
     </>
   );
 }
+
+ContentsSection.propTypes = {
+  titleClass: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  contents: PropTypes.string.isRequired,
+  opportunities: PropTypes.instanceOf(Array),
+};
 
 export default ContentsSection;
