483c2bdc408eb8cbb0f873c99455757cfe6eeeb4
Aurangzaib Najam <Aurangzaib Najam>
2023-05-15 19:59:31 +0500

    Creates contents section component

---
+++ src/components/WorkWithMe/ContentsSection.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ContentsSection() {
+  return ();
+}
+
+export default ContentsSection;
