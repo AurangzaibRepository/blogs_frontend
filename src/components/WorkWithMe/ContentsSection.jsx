b6ba26ef56f5de33604d9e73c0ee88abef66855b
Aurangzaib Najam <Aurangzaib Najam>
2023-05-15 20:06:43 +0500

    Specifies props

--- src/components/WorkWithMe/ContentsSection.jsx
+++ src/components/WorkWithMe/ContentsSection.jsx
@@ -2,8 +2,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function ContentsSection() {
-  return ();
+function ContentsSection({
+  titleClass,
+  title,
+  contents,
+  opportunities
+}) {
+  return (
+    <>
+      { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
+      <label className={"full-width"}></label>
+    </>
+  );
 }
 
 export default ContentsSection;
