e8ec31d5d0e6105588e091cbd507ba6e7b8c1c53
Aurangzaib Najam <Aurangzaib Najam>
2023-05-15 22:33:53 +0500

    Adds divs

--- src/components/RoadToReact/index.jsx
+++ src/components/RoadToReact/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function RoadToReact() {
-  return();
+  return (
+    <div className="row">
+      <div className="col-md-6"></div>
+      <div className="col-md-6"></div>
+    </div>
+  );
 }
 
 export default RoadToReact;
