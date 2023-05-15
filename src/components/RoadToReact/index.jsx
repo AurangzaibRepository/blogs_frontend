1725f7623764b0dbd75c0b74bdf2e42afc52ba26
Aurangzaib Najam <Aurangzaib Najam>
2023-05-15 22:46:18 +0500

    Adds label

--- src/components/RoadToReact/index.jsx
+++ src/components/RoadToReact/index.jsx
@@ -7,7 +7,10 @@ function RoadToReact() {
       <div className="col-md-6">
         <img src={imgReactBook} alt="react book" />
       </div>
-      <div className="col-md-6"></div>
+      <div className="col-md-6">
+        { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
+        <label className="full-width title text-center">The Road To React</label>
+      </div>
     </div>
   );
 }
