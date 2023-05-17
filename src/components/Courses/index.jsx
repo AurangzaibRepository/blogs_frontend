f77b306a5abe1d726a4193c53d4ebc37f6c5b8d2
Aurangzaib Najam <Aurangzaib Najam>
2023-05-17 20:30:01 +0500

    Adds images

--- src/components/Courses/index.jsx
+++ src/components/Courses/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Title from '../layouts/Title';
+import imgReact from '../../assets/road-to-react.png';
+import imgRedux from '../../assets/road-to-redux.png';
+import imgGraphQL from '../../assets/road-to-graphql.png';
 import './style.css';
 
 function Courses() {
@@ -13,18 +16,21 @@ function Courses() {
               <div className="dv-box">
                 { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
                 <label>The Road to React</label>
+                <img src={imgReact} alt="react" />
               </div>
             </div>
             <div className="col-md-4">
               <div className="dv-box">
                 { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
                 <label>The Road to Redux</label>
+                <img src={imgRedux} alt="redux" />
               </div>
             </div>
             <div className="col-md-4">
               <div className="dv-box">
                 { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
                 <label>The Road to GraphQL</label>
+                <img src={imgGraphQL} alt="graphql" />
               </div>
             </div>
           </div>
