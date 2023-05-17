07fac0e27e5f0ecb820ecc04171102f70b7302e8
Aurangzaib Najam <Aurangzaib Najam>
2023-05-17 19:45:50 +0500

    Adds div inside main div

--- src/components/Courses/index.jsx
+++ src/components/Courses/index.jsx
@@ -3,21 +3,23 @@ import './style.css';
 
 function Courses() {
   return (
-    <div className="container dv-main" id="dv-courses">
-      <div className="row">
-        <div className="col-md-4">
-          <div className="dv-box">
-
+    <div className="dv-main" id="dv-courses">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="dv-box">
+              dcfsd
+            </div>
           </div>
-        </div>
-        <div className="col-md-4">
-          <div className="dv-box">
-            
+          <div className="col-md-4">
+            <div className="dv-box">
+              sdf
+            </div>
           </div>
-        </div>
-        <div className="col-md-4">
-          <div className="dv-box">
-            
+          <div className="col-md-4">
+            <div className="dv-box">
+              sdfsd
+            </div>
           </div>
         </div>
       </div>
