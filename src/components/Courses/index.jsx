f8bec721c1ff96afd92167ef573559b12dd6970c
Aurangzaib Najam <Aurangzaib Najam>
2023-05-17 19:57:53 +0500

    Includes title component

--- src/components/Courses/index.jsx
+++ src/components/Courses/index.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
+import Title from '../layouts/Title';
 import './style.css';
 
 function Courses() {
   return (
-    <div className="dv-main" id="dv-courses">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <div className="dv-box">
-              dcfsd
+    <>
+      <Title title="Courses" />
+      <div className="dv-main" id="dv-courses">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="dv-box">
+                dcfsd
+              </div>
             </div>
-          </div>
-          <div className="col-md-4">
-            <div className="dv-box">
-              sdf
+            <div className="col-md-4">
+              <div className="dv-box">
+                sdf
+              </div>
             </div>
-          </div>
-          <div className="col-md-4">
-            <div className="dv-box">
-              sdfsd
+            <div className="col-md-4">
+              <div className="dv-box">
+                sdfsd
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
