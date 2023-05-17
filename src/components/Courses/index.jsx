6a1f385249af52b7bcf2bae0957340cbb5362f98
Aurangzaib Najam <Aurangzaib Najam>
2023-05-17 19:18:37 +0500

    Adds divs

--- src/components/Courses/index.jsx
+++ src/components/Courses/index.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import './style.css';
 
 function Courses() {
-  return(); 
+  return(
+    <div className="container dv-main">
+      <div className="row">
+        <div className="col-md-4">
+          <div className="dv-box">
+
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="dv-box">
+            
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="dv-box">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Courses;
