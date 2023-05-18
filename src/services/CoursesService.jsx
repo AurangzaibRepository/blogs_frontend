0d39e6b18ca42e3bca8352cae14c095729cbfe8b
Aurangzaib Najam <Aurangzaib Najam>
2023-05-18 19:56:32 +0500

    Implements function to get data in service

--- src/services/CoursesService.jsx
+++ src/services/CoursesService.jsx
@@ -1,5 +1,10 @@
-const CoursesService {
-  
+const CoursesService = {
+  getData: async () => {
+    let data = await fetch('data/courses_data.json');
+    data = await data.json();
+
+    return data.courses;
+  },
 };
 
 export default CoursesService;
