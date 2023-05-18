26e5336363694cb658a39e99bc76131140191d15
Aurangzaib Najam <Aurangzaib Najam>
2023-05-18 17:20:33 +0500

    Includes div and label

--- src/components/Courses/Box.jsx
+++ src/components/Courses/Box.jsx
@@ -1,13 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 function Box({ title, icon }) {
-  return ();
+  return (
+    <div className="dv-box">
+      { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
+      <label className="title">{title}</label>
+    </div>
+  );
 }
 
 Box.propTypes = {
   title: PropTypes.string.isRequired,
-  icon: PropTypes.object,
+  icon: PropTypes.instanceOf(Object).isRequired,
 }
 
 export default Box;
