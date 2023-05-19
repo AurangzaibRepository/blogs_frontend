4b4c3454d6c80576bbff4cd6a35a820984a64899
Aurangzaib Najam <Aurangzaib Najam>
2023-05-19 21:07:01 +0500

    Implements service function to retreieve data

--- src/services/LegalService.jsx
+++ src/services/LegalService.jsx
@@ -1,5 +1,10 @@
 const LegalService = {
+  getData: async () => {
+    let data = await fetch('data/legal_data.json');
+    data = await data.json();
 
+    return data;
+  },
 };
 
 export default LegalService;
