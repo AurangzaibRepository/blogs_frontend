0b4f372ce4ebbb18a5c80b006a84a05e04215b3f
Aurangzaib Najam <Aurangzaib Najam>
2023-05-19 21:13:06 +0500

    Adds label and p elements

--- src/components/Legal/index.jsx
+++ src/components/Legal/index.jsx
@@ -6,6 +6,9 @@ function Legal() {
     <>
       <Title title="Legal Notice and Terms" />
       <div className="container dv-main">
+        { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
+        <label className="full-width title text-center"></label>
+        <p className="re-line"></p>
       </div>
     </>
   );
