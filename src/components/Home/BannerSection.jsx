b5e47f091b0ed2afb4085ececf6a7e59ef6816a4
Aurangzaib Najam <Aurangzaib Najam>
2023-05-25 20:28:56 +0500

    Adds body elements

--- src/components/Home/BannerSection.jsx
+++ src/components/Home/BannerSection.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function BannerSection() {
-  return ();
+  return (
+    <div id="dv-banner" style={{ backgroundImage: `url(${imgBanner})` }}>
+      <div className="text-center">
+        { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
+        <label id="label-title">Robin Wieruch</label>
+        <br />
+        <span id="span-subtitle">German Software Engineer for React.js, Node.js and GraphQL</span>
+      </div>
+    </div>
+  );
 }
 
 export default BannerSection;
