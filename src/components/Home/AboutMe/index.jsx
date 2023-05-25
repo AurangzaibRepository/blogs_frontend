3bc46965758915ce525da0ff1f523c5f235b3896
Aurangzaib Najam <Aurangzaib Najam>
2023-05-25 23:40:54 +0500

    Specifies id

--- src/components/Home/AboutMe/index.jsx
+++ src/components/Home/AboutMe/index.jsx
@@ -10,7 +10,7 @@ function AboutMeSection({
   description,
 }) {
   return (
-    <div>
+    <div id="dv-aboutme">
       <div className="text-center">
         { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
         <label className="title mb-0">{title}</label>
