cb6cabf0aca9a10d9d5f99cbbadf503942e9395d
Aurangzaib Najam <Aurangzaib Najam>
2023-05-25 23:39:48 +0500

    Renders icons

--- src/components/Home/AboutMe/Icons.jsx
+++ src/components/Home/AboutMe/Icons.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import BrandIcon from '../../Footer/BrandIcon';
 import { faMailBulk } from '@fortawesome/free-solid-svg-icons';
 import {
   faTwitter,
   faGit,
   faFacebook,
 } from '@fortawesome/free-brands-svg-icons';
+import BrandIcon from '../../Footer/BrandIcon';
 
 function Icons() {
   return (
     <div className="mt-3">
-      
+      <BrandIcon
+        className="me-4"
+        icon={faTwitter}
+      />
+      <BrandIcon
+        className="me-4"
+        icon={faGit}
+      />
+      <BrandIcon
+        className="me-4"
+        icon={faFacebook}
+      />
+      <BrandIcon
+        icon={faMailBulk}
+      />
     </div>
   );
 }
