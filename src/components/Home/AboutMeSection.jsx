b1736ab1eded41cf1dfbbe076b86dfc6e1718d60
Aurangzaib Najam <Aurangzaib Najam>
2023-05-25 21:34:41 +0500

    Adds title and subtitle

--- src/components/Home/AboutMeSection.jsx
+++ src/components/Home/AboutMeSection.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function AboutMeSection() {
-  return ();
+  return (
+    <div>
+      <div className="text-center">
+        { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
+        <label className="title"></label>
+        <br />
+        <span id="span-subtitle"></span>
+      </div>
+    </div>
+  );
 };
 
 export default AboutMeSection;
