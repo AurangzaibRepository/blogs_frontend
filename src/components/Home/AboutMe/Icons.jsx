44d96bd89e9930178387c3c7ab74b90505b6b8a1
Aurangzaib Najam <Aurangzaib Najam>
2023-05-25 23:28:25 +0500

    Imports brand icon component

--- src/components/Home/AboutMe/Icons.jsx
+++ src/components/Home/AboutMe/Icons.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import BrandIcon from '../../Footer/BrandIcon';
 
 function Icons() {
-  return ();
+  return (
+    <div className="mt-3">
+      
+    </div>
+  );
 }
 
 export default Icons;
