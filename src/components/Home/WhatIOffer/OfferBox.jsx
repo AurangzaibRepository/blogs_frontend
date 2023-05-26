a5a52f9eeb3bd07ae0392347a181ae55cd173044
Auranzgaib Najam <Aurangzaib Najam>
2023-05-26 20:07:45 +0500

    Adds body

--- src/components/Home/WhatIOffer/OfferBox.jsx
+++ src/components/Home/WhatIOffer/OfferBox.jsx
@@ -3,7 +3,15 @@ import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function OfferBox() {
-  return ();
+  return (
+    <div className="col-md-4 mt-5" key={item.id}>
+      <FontAwesomeIcon icon={} />
+      <br />
+      { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
+      <label className="sub-title mb-4 mt-0">{item.title}</label>
+      <p>{}</p>
+    </div>
+  );
 }
 
 export default OfferBox;
