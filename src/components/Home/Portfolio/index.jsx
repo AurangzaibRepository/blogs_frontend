3147fc26664df1fd724568588f03062cdf805eb1
Auranzgaib Najam <Aurangzaib Najam>
2023-05-26 21:54:37 +0500

    Adds body

--- src/components/Home/Portfolio/index.jsx
+++ src/components/Home/Portfolio/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function PortfolioSection() {
-  return ();
+  return (
+    <div id="dv-portfolio">
+      <div className="text-center">
+        { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
+        <label className="title mb-0"></label>
+        <br />
+        <span id="span-subtitle"></span>
+      </div>
+    </div>
+  );
 }
 
 export default PortfolioSection;
