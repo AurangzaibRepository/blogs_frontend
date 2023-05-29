478a91f474b0a1d5d3870d28056816812a17aa0b
Aurangzaib Najam <Aurangzaib Najam>
2023-05-29 18:09:31 +0500

    Impots portfolio box component

--- src/components/Home/Portfolio/index.jsx
+++ src/components/Home/Portfolio/index.jsx
@@ -2,37 +2,20 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
-import Portfolio1 from '../../../assets/home/portfolio1.jpg';
-import Portfolio2 from '../../../assets/home/portfolio2.jpg';
-import Portfolio3 from '../../../assets/home/portfolio3.jpg';
+import PortfolioBox from './PortfolioBox';
 
 function PortfolioSection({
   title,
   caption,
   items,
 }) {
-  const imageList = [
-    Portfolio1,
-    Portfolio2,
-    Portfolio3,
-  ];
-
   return (
     <div id="dv-portfolio">
       <div className="text-center">
         <label className="title mb-0">{title}</label>
         <br />
         <span id="span-subtitle">{caption}</span>
-        <div className="row mt-4 pt-2 justify-content-center">
-          {items.map((item, index) => (
-            <div className="col-md-4 mt-4">
-              <div className="portfolio-box">
-                <img src={imageList[index]} alt="Portfolio1" />
-                <label className="image-title">{item.title}</label>
-              </div>
-            </div>
-          ))}
-        </div>
+
       </div>
     </div>
   );
