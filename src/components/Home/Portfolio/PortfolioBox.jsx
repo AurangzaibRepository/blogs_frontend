33c447db0a51610285edc7a050e98b04a5f811cf
Aurangzaib Najam <Aurangzaib Najam>
2023-05-29 18:05:28 +0500

    Adds body

--- src/components/Home/Portfolio/PortfolioBox.jsx
+++ src/components/Home/Portfolio/PortfolioBox.jsx
@@ -1,8 +1,21 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 function PortfolioBox({ items }) {
-  return();
+  return (
+    <div className="row mt-4 pt-2 justify-content-center">
+      {items.map((item) => (
+        <div className="col-md-4 mt-4">
+          <div className="portfolio-box">
+            <img src={imageList[index]} alt="Portfolio1" />
+            <label className="image-title">{item.title}</label>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 PortfolioBox.propTypes = {
