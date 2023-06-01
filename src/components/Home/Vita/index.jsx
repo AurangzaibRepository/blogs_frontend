a835b68c77b2c131bcb2b3553a4aad6c044542a9
Aurangzaib Najam <Aurangzaib Najam>
2023-06-01 19:55:04 +0500

    Renders title and caption

--- src/components/Home/Vita/index.jsx
+++ src/components/Home/Vita/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
@@ -6,6 +7,9 @@ function VitaSection({ title, caption }) {
   return (
     <div id="dv-vita">
       <div className="text-center">
+        <label className="title mb-0">{title}</label>
+        <br />
+        <span id="span-subtitle">{caption}</span>
       </div>
     </div>
   );
