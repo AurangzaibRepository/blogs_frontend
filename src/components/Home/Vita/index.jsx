63dc7ca980a03a0312ec0a921029538b30749f27
Aurangzaib Najam <Aurangzaib Najam>
2023-06-01 22:00:19 +0500

    Render items via box component

--- src/components/Home/Vita/index.jsx
+++ src/components/Home/Vita/index.jsx
@@ -2,27 +2,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
-import Vita1 from '../../../assets/home/vita1.jpg';
-import Vita2 from '../../../assets/home/vita2.jpg';
-import Vita3 from '../../../assets/home/vita3.jpg';
-import Vita4 from '../../../assets/home/vita4.jpg';
-import Vita5 from '../../../assets/home/vita5.jpg';
-import Vita6 from '../../../assets/home/vita6.jpg';
-import Vita7 from '../../../assets/home/vita7.jpg';
-import Vita8 from '../../../assets/home/vita8.jpg';
-import Vita9 from '../../../assets/home/vita9.jpg';
-
-const imageList = [
-  Vita1,
-  Vita2,
-  Vita3,
-  Vita4,
-  Vita5,
-  Vita6,
-  Vita7,
-  Vita8,
-  Vita9,
-];
+import VitaBox from './VitaBox';
 
 function VitaSection({
   title,
@@ -35,18 +15,7 @@ function VitaSection({
         <label className="title mb-0">{title}</label>
         <br />
         <span id="span-subtitle">{caption}</span>
-        <div className="row mt-4">
-          {items.map((item, index) => (
-            <div className="col-md-4 mt-5">
-              <img src={imageList[index]} alt="Vita" />
-              <br />
-              <label className="sub-title mb-0 label-year">{item.year}</label>
-              <br />
-              <label className="sub-title mt-0 mb-4">{item.title}</label>
-              <p>{item.description}</p>
-            </div>
-          ))}
-        </div>
+        <VitaBox items={items} />
       </div>
     </div>
   );
