eb2abd8cb31d18f2102957db2c57b65f4a330b74
Aurangzaib Najam <Aurangzaib Najam>
2023-06-01 21:08:03 +0500

    Imports images

--- src/components/Home/Vita/VitaBox.jsx
+++ src/components/Home/Vita/VitaBox.jsx
@@ -1,5 +1,14 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import Vita1 from '../../../assets/home/vita1.jpg';
+import Vita2 from '../../../assets/home/vita2.jpg';
+import Vita3 from '../../../assets/home/vita3.jpg';
+import Vita4 from '../../../assets/home/vita4.jpg';
+import Vita5 from '../../../assets/home/vita5.jpg';
+import Vita6 from '../../../assets/home/vita6.jpg';
+import Vita7 from '../../../assets/home/vita7.jpg';
+import Vita8 from '../../../assets/home/vita8.jpg';
+import Vita9 from '../../../assets/home/vita9.jpg';
 
 function VitaBox() {
   return (
