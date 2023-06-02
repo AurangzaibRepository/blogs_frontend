a456b69741933d37995f6de013a90eb5eff5f091
Aurangzaib Najam <Aurangzaib Najam>
2023-06-03 00:19:10 +0500

    Imports blog box

--- src/components/Blogs/BlogList.jsx
+++ src/components/Blogs/BlogList.jsx
@@ -2,22 +2,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
-import Blog from '../../assets/blogs/blog.png';
+import BlogBox from './BlogBox';
 
 function BlogList({ blogs }) {
   return (
     <>
       {blogs.map((item) => (
-        <div className="dv-blog mt-4 row" key={item.id}>
-          <div className="col-md-4 dv-image">
-            <img src={Blog} alt="Blog" />
-          </div>
-          <div className="col-md-8">
-            <label className="label-blogtitle sub-title mt-0">{item.title}</label>
-            <br />
-            <p>{item.description}</p>
-          </div>
-        </div>
+        
       ))}
     </>
   );
