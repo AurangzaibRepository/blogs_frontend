257bacb30f6ccd68fedf0e07bc28bcf32d47aed7
Aurangzaib Najam <Aurangzaib Najam>
2023-06-02 20:23:17 +0500

    Creates service function to get data

--- src/services/BlogsService.jsx
+++ src/services/BlogsService.jsx
@@ -1,5 +1,10 @@
 const BlogsService = {
+  getData: async () => {
+    let data = await fetch('data/blogs_data.json');
+    data = await data.json();
 
+    return data;
+  },
 };
 
 export default BlogsService;
