a2989a465e539870590d075a2d6302d6a11006ea
Aurangzaib Najam <Aurangzaib Najam>
2023-06-02 23:55:43 +0500

    Adds title and description

--- src/components/Blogs/BlogList.jsx
+++ src/components/Blogs/BlogList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
@@ -7,8 +8,15 @@ function BlogList({ blogs }) {
   return (
     <>
       {blogs.map((item) => (
-        <div className="dv-blog mt-4">
-          <img src={Blog} alt="Blog" />
+        <div className="dv-blog mt-4 row" key={item.id}>
+          <div className="col-md-4 dv-image">
+            <img src={Blog} alt="Blog" />
+          </div>
+          <div className="col-md-8">
+            <label className="label-blogtitle">{item.title}</label>
+            <br />
+            <p>{item.description}</p>
+          </div>
         </div>
       ))}
     </>
